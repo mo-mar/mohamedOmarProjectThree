e1396c96c7a4c7a8c919d60e77d84fb0895e8639
Mohamed Omar <mohamedsomar1@gmail.com>
2019-08-24 13:42:16 -0400

    finally fixed the username issue

--- script.js
+++ script.js
@@ -50,6 +50,9 @@ myApp.userPressedNo = function (event) {
 myApp.showResultYes = function(){
     $('.yes').on('click touch', 'input#submitYesQuiz1', function(e){
         e.preventDefault();
+        myApp.yesResult1();
+        myApp.yesResult2();
+        myApp.yesResult3();
         console.log(myApp.userName);
         let userChoice = $('#yesQuiz1 input[type=radio][name=yesQuiz1]:checked').val();
         if (userChoice === 'yes1'){
@@ -71,6 +74,9 @@ myApp.showResultYes = function(){
 myApp.showResultNo = function() {
     $('.no').on('click', 'input#submitNoQuiz1', function (e) {
         e.preventDefault();
+        myApp.noResult1();
+        myApp.noResult2();
+        myApp.noResult3();
         let userChoice = $('#noQuiz1 input[type=radio][name=noQuiz1]:checked').val();
         console.log(userChoice);
         if (userChoice === 'noResponse1') {
@@ -100,7 +106,8 @@ myApp.yesQuizQuestion1 = `<form action=""><fieldset>
                             </fieldset>
                         </form>`;
 // this is a property containing the html markup for the first question in the NO path
-myApp.noQuizQuestion1 = `<form action=""><fieldset>
+myApp.noQuizQuestion1 = function(){
+return `<form action=""><fieldset>
                                 <legend>Uh, what? Did you just say you didn't vote, ${myApp.userName}?</legend>
                                 <div><input type="radio" name="noQuiz1" id="no1" value="noResponse1">
                                 <label for="no1">You heard me. I'm throwing my vote away.</label></div>
@@ -112,30 +119,43 @@ myApp.noQuizQuestion1 = `<form action=""><fieldset>
                                 <input type="submit" id="submitNoQuiz1" name="noQuiz1" value="Submit">
                             </fieldset>
                         </form>`;
+}
 // these are the three possible results in the YES path
 
-myApp.yesResult1 = `<h2>Whoa! You've fulfilled your basic responsibilities as a citizen! Congratulations, ${myApp.userName}!</h2> <p>Why not go above and beyond and remind your friends to vote?</p><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+myApp.yesResult1 = function(){
+    return `<h2>Whoa! You've fulfilled your basic responsibilities as a citizen! Congratulations, ${myApp.userName}!</h2> <p>Why not go above and beyond and remind your friends to vote?</p><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+}
 
-myApp.yesResult2 = `<h2>Nice! You've fulfilled your basic responsibilities as a citizen.</h2><p>And if that wasn't enough, you also refuse to partake in social media trends! Congratulations, ${myApp.userName}!</p><p>Why not go above and beyond and remind your friends to vote?</p><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+myApp.yesResult2 = function(){
+    return `<h2>Nice! You've fulfilled your basic responsibilities as a citizen.</h2><p>And if that wasn't enough, you also refuse to partake in social media trends! Congratulations, ${myApp.userName}!</p><p>Why not go above and beyond and remind your friends to vote?</p><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+}
 
-myApp.yesResult3 = `<h2>Are you telling me you respect democracy <em>and</em> an individual's right to privacy?</h2> <p>Wow, ${myApp.userName}, you're a pillar of hope for society!</p><p>Why not go above and beyond and remind your friends to vote?</p><br><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+myApp.yesResult3 = function(){
+    return `<h2>Are you telling me you respect democracy <em>and</em> an individual's right to privacy?</h2> <p>Wow, ${myApp.userName}, you're a pillar of hope for society!</p><p>Why not go above and beyond and remind your friends to vote?</p><br><i class="fab fa-twitter"></i><a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Whoa! I'm a responsible citizen, according to https://www.HaveYouVotedYet.ca!">Share this achievement on Twitter!</a>`;
+}
 
 
 // this the HTML markup for when user finishes the NO path
 
-myApp.noResult1 = `<h2 class="noResultDynamic">Your mother and I are extremely disappointed in you, ${myApp.userName}.</h2><p>It's not too late, so get out there and VOTE! You can find all the important information on what kind of ID you need and where to go below. GET TO IT!</p><a href="https://www.elections.ca/homeGE.aspx">Find all the info you need on the federal election here.</a>
+myApp.noResult1 = function(){
+    return `<h2 class="noResultDynamic">Your mother and I are extremely disappointed in you, ${myApp.userName}.</h2><p>It's not too late, so get out there and VOTE! You can find all the important information on what kind of ID you need and where to go below. GET TO IT!</p><a href="https://www.elections.ca/homeGE.aspx">Find all the info you need on the federal election here.</a>
 <a href="https://www.elections.ca/content2.aspx?section=reg&document=index&lang=e">Not sure if you're registered? Go here, POST HASTE.</a>
 <a href="https://www.elections.ca/homeGE.aspx">Want to know which riding your in? This tool is your friend.</a>`;
+}
 
-myApp.noResult2 = `<h2>Too busy. Got it. OK. Makes sense. We all lead busy lives I suppo-</h2><br><p>WHAT DO YOU MEAN YOU'RE TOO BUSY, ${myApp.userName} ? Get out there and VOTE! You can find all the info on what kind of ID you need and where to go below. GET TO IT!</p><a href="https://www.elections.ca/homeGE.aspx">Find all the info you need on the federal election here.</a>
+myApp.noResult2 = function(){
+    return `<h2>Too busy. Got it. OK. Makes sense. We all lead busy lives I suppo-</h2><br><p>WHAT DO YOU MEAN YOU'RE TOO BUSY, ${myApp.userName} ? Get out there and VOTE! You can find all the info on what kind of ID you need and where to go below. GET TO IT!</p><a href="https://www.elections.ca/homeGE.aspx">Find all the info you need on the federal election here.</a>
 <a href="https://www.elections.ca/content2.aspx?section=reg&document=index&lang=e">Not sure if you're registered? Go here, POST HASTE.</a>
 <a href="https://www.elections.ca/homeGE.aspx">Want to know which riding your in? This tool is your friend.</a>`;
+}
 
-myApp.noResult3 = `<h2>Really, ${myApp.userName}? I know you don't mean that.</h2> 
+myApp.noResult3 = function(){
+    return `<h2>Really, ${myApp.userName}? I know you don't mean that.</h2> 
 <p>You're just afraid of greatness, aren't you?
 Now get out there and VOTE! You can find all the info on what kind of ID you need and where to go below. GET TO IT!</p><a href="https://www.elections.ca/homeGE.aspx">Find all the info you need on the federal election here.</a>
 <a href="https://www.elections.ca/content2.aspx?section=reg&document=index&lang=e">Not sure if you're registered? Go here, POST HASTE.</a>
 <a href="https://www.elections.ca/homeGE.aspx">Want to know which riding your in? This tool is your friend.</a>`;
+}
 
 myApp.init = function(){
     myApp.submitLandingForm();
@@ -143,7 +163,6 @@ myApp.init = function(){
     myApp.userPressedNo();
     myApp.showResultYes();
     myApp.showResultNo();
-    
 }
 
 $(document).ready(function(){
